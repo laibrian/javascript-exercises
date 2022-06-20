e21d3acf8f412fa67e7fa0779e117b52489140c4
Brian Lai <laivbrian@gmail.com>
2022-06-20 15:28:06 -0500

    Complete js Exercise 03 - reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversedStr = '';
+    for (let i = str.length; i > 0; i--)  {
+        let currentChar = str.slice(i-1, i);
+        reversedStr += currentChar;
+    }
+    return reversedStr;
 };
 
 // Do not edit below this line
