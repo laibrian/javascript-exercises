c3ca3df26f3142d12f315c3d574cbf55ade83c90
Brian Lai <laivbrian@gmail.com>
2022-06-20 17:02:42 -0500

    Complete js Exercise 07 - tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  // formula to convert F to C
+  let celsius = (fahrenheit - 32) * 5 / 9;
+  // reformat conversion results to be rounded to one decimal place (becomes a String type)
+  celsius = (Math.round(celsius * 10) / 10).toFixed(1);
+  // return reformatted conversion as a Number type
+  return Number(celsius);
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  // formula to convert C to F
+  let fahrenheit = celsius * 9 / 5 + 32;
+  // reformat conversion results to be rounded to one decimal place (becomes a String type)
+  fahrenheit = (Math.round(fahrenheit * 10) / 10).toFixed(1);
+  // return reformatted conversion as a Number type
+  return Number(fahrenheit);
 };
 
 // Do not edit below this line
