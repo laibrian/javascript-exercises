8d8cd42b0ad6b0cd8b615fd38f8e726874c3c9af
Brian Lai <laivbrian@gmail.com>
2022-06-20 15:11:07 -0500

    Complete js Exercise 02 - repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, numOfRepeats) {
+    let newStr = '';
+    if (!(numOfRepeats < 0))  {
+        for (let i = 0; i < numOfRepeats; i++) {
+            newStr += str;
+        }
+        return newStr;
+    }
+    return 'ERROR';
 };
 
 // Do not edit below this line
