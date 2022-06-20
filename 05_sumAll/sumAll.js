b9e12a1c2e4e323e38c6c79526a7db64aaac9005
Brian Lai <laivbrian@gmail.com>
2022-06-20 16:16:38 -0500

    Complete js Exercise 05 - sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
 
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+
+    if (!(start < 0) && !(end < 0) && (typeof start === 'number') && (typeof end === 'number')) {
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
+        return sum
+    }
+    return 'ERROR';
 };
 
 // Do not edit below this line
