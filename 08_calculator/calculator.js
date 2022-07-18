d983d637ae835ad36dc2477ecbb8b4253745ef3a
Brian Lai <laivbrian@gmail.com>
2022-07-18 12:32:53 -0500

    Complete js Exercise 08 - calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let arraySum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    arraySum += array[i];
+  }
+
+  return arraySum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let result = 1;
 
+  for (let i = 0; i < array.length; i++) {
+    result = array[i] * result;
+  }
+  
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let result = 1;
+
+  for (let i = 0; i < b; i++) {
+    result = a * result;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = 1;
+
+  for (let i = 1; i < a + 1; i++) {
+    result = i * result;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
