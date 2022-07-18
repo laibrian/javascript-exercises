6472887826ed91663d62e7221934601b59e68d62
Brian Lai <laivbrian@gmail.com>
2022-07-18 14:18:58 -0500

    Complete js Exercise 10 - fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    
+    if (member < 0) {
+        return "OOPS"; // no negatives
+    }   
 
+    let n1 = 0;
+    let n2 = 1;
+    let nextTerm;
+
+    for (let i = 1; i <= member; i++) {
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+    }
+
+    return n1;
 };
 
 // Do not edit below this line
